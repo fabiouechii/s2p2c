04ba0f509f798dde1bbc40803f4b1110997c8679
fabiouechii <fabiouechi19@gmail.com>
2024-10-28 17:18:43 -0300

    novo e lista funcionando
    atualizado funcao novo 
    corrigido funcao lista para leitura do binario

--- main.c
+++ main.c
@@ -29,38 +29,48 @@ int menu(){
 
 //---------------------------------------------------------------------------------------
 
-int lista(){
-    printf("\n\nEsta e a sua lista de tarefas:\n");
+int lista(const char* arq){
+    FILE *file = fopen(arq, "rb");
+    if (!file) {
+        printf("erro\n");
+        return -1;
+    }
 
-    char lista[MAX_SIZE][50];
-    int tamanho = 0;
-
-    strcpy(lista[tamanho++], "\nTitulo: Teste nome");
-    strcpy(lista[tamanho++], "Data de criacao: 10/10/2024");
-    strcpy(lista[tamanho++], "Porcentagem: 80%");
-
-    printf("valor 1: ", lista[1]);
-
-    for(int i = 0; i < tamanho; i++) {
-        printf("%s\n", lista[i]);
-    }    
-
-    return 0;
+    tarefa c;
+    int index = 0;
+    printf("Lista de Tarefas:\n");
+    while (fread(&c, sizeof(tarefa), 1, file) == 1) {
+        printf("%d. Titulo: %s, Data de Inicio: %s, Data de Encerramento: %s, Porcentagem: %s\n",
+            index+1, c.titulo, c.data_inicio, c.data_encerramento, c.porcentagem);
+        index++;
+    }
+    
+    fclose(file);
+    menu();
 }
     
-int novo(){
-    int tit;
-    int ini;
-    int deadline;
-    int percent;
-    printf("\nTITULO:");
-    scanf("%d", &tit);
-    printf("\nDATA DE INICIO:");
-    scanf("%d", &ini);
-    printf("\nDATA DE ENCERRAMENTO:");
-    scanf("%d", &deadline);
-    printf("\nPORCENTAGEM FEITA:");
-    scanf("%d", &percent);
+void novo(const char* arq) {
+    tarefa c;
+    FILE *file = fopen(arq, "ab");
+
+    if (file) {
+        printf("\nTITULO: ");
+        scanf(" %[^\n]", c.titulo);
+        printf("\nDATA DE INICIO: ");
+        scanf(" %[^\n]", c.data_inicio);
+        printf("\nDATA DE ENCERRAMENTO: ");
+        scanf(" %[^\n]", c.data_encerramento);
+        printf("\nPORCENTAGEM FEITA: ");
+        scanf(" %[^\n]", c.porcentagem);
+
+        fwrite(&c, sizeof(tarefa), 1, file); //escreve no arquivo
+        
+        fclose(file);
+        printf("\nadicionada\n");
+        menu();
+    } else {
+        printf("\nerro\n");
+    }
 }
 
 int edit(){
